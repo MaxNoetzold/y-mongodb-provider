777bb7746e767eaed8b443438955b85e091dddd4
MaxNoetzold <max.noetzold@gmail.com>
2022-09-04 14:47:53 +0200

    fixed dist sript

--- rollup.config.js
+++ rollup.config.js
@@ -46,7 +46,7 @@ export default [
       paths: path => {
         if (/^lib0\//.test(path)) {
           //return `lib0/dist/${path.slice(5, -3)}.cjs
-		  return `lib0/dist/${path}.cjs`
+		  return `lib0/dist/${path.slice(5)}.cjs`
         }
         return path
       }
